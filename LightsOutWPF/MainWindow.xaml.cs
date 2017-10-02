77c6add74b2158d5ac320118d47fa471fb51cb7e
Christian <cellison2@harding.edu>
2017-10-02 10:44:00 -0600

    Start

--- LightsOutWPF/MainWindow.xaml.cs
+++ LightsOutWPF/MainWindow.xaml.cs
@@ -20,9 +20,99 @@ namespace LightsOutWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private LightsOutGame game;
         public MainWindow()
         {
             InitializeComponent();
+            game = new LightsOutGame();
+            CreateGrid();
+            DrawGrid();
+        }
+
+        private void CreateGrid()
+        {
+            int rectSize = (int)boardCanvas.Width / game.NumCells;
+            // Create rectangles for grid
+            for (int r = 0; r < game.NumCells; r++)
+            {
+                for (int c = 0; c < game.NumCells; c++)
+                {
+                    Rectangle rect = new Rectangle();
+                    rect.Fill = Brushes.White;
+                    rect.Width = rectSize + 1;
+                    rect.Height = rect.Width + 1;
+                    rect.Stroke = Brushes.Black;
+                    // Store each row and col as a Point
+                    rect.Tag = new Point(r, c);
+                    // Register event handler
+                    rect.MouseLeftButtonDown += Rect_MouseLeftButtonDown;
+                    // Put the rectangle at the proper location within the canvas
+                    Canvas.SetTop(rect, r * rectSize);
+                    Canvas.SetLeft(rect, c * rectSize);
+                    // Add the new rectangle to the canvas' children
+                    boardCanvas.Children.Add(rect);
+                }
+            }
+        }
+        private void Rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle rect = sender as Rectangle;
+            var rowCol = (Point)rect.Tag;
+            int row = (int)rowCol.X;
+            int col = (int)rowCol.Y;
+
+            game.MakeMove(row, col);
+            DrawGrid();
+        }
+        private void DrawGrid()
+        {
+            int index = 0;
+
+            // Set the colors of the rectangles
+            for (int r = 0; r < game.NumCells; r++)
+            {
+                for (int c = 0; c < game.NumCells; c++)
+                {
+                    Rectangle rect = boardCanvas.Children[index] as Rectangle;
+                    index++;
+                    if (game.GetGridValue(r, c))
+                    {
+                        // On
+                        rect.Fill = Brushes.White;
+                        rect.Stroke = Brushes.Black;
+                    }
+                    else
+                    {
+                        // Off
+                        rect.Fill = Brushes.Black;
+                        rect.Stroke = Brushes.White;
+                    }
+                }
+            }
+        }
+        private void MenuExit_Click()
+        {
+            Close();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if(sender == AboutMenuItem)
+            {
+                AboutWindow aboutForm = new AboutWindow();
+                aboutForm.ShowDialog();
+            }
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void NewGame_Click(object sender, RoutedEventArgs e)
+        {
+            game.NewGame();
+            DrawGrid();
         }
     }
 }
